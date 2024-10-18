41e81ea1b89d9b7b796383f7c3dc346e1adc3da0
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-18 10:15:27 +0600

    handle audio and file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header() {
     return (
         <header className="flex items-center justify-between gap-4 p-4">
-            <h1>Tran<span className='text-blue-400'>Scribe</span></h1>
-            <button className='flex items-center gap-2'><p>New</p><i className="fa-solid fa-plus"></i></button>
+            <h1 className='font-medium'>Easy<span className='text-blue-400 bold'>Scribe</span></h1>
+            <button className='flex items-center gap-2 spacialBtn px-4 py-2 rounded-lg text-blue-400'><p>New</p><i className="fa-solid fa-plus"></i></button>
         </header>
     )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export default function HomePage() {
-    return (
-        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center bg-green-400'>
+export default function HomePage(props) {
 
+    const { setAudioStream, setFile } = props
+
+    return (
+        <main className='flex-1 p-4 flex flex-col text-center gap-3 sm:gap-4 md:gap-5 justify-center pb-20'>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Easy<span className='text-blue-400 bold'>Scribe</span></h1>
+            <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span>&rarr;</span> Translate</h3>
+            <button className='flex spacialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
+                <p className='text-blue-400'>Record</p>
+                <i className="fa-solid fa-microphone"></i>
+            </button>
+            <p className='text-base'>Or <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'>upload
+            <input onChange={(e)=>{
+                const tempFile = e.target.files(0)
+                setFile(tempFile)
+            }} className='hidden' type="file" name="" id="" accept='.mp3,.wave' /></label> a mp3 file</p>
+            <p className='italic text-slate-500'>Free now free forver</p>
         </main>
     )
 }
