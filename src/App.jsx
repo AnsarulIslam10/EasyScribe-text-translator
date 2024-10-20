31aba0e9e860d6ef2532906d2450b72d8ebafe79
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-20 12:04:53 +0600

    transcribing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState, useEffect } from 'react'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
 import FileDisplay from './components/FileDisplay'
+import Information from './components/Information'
+import Transcribing from './components/Transcribing'
 
 
 function App() {
   const [file, setFile] = useState(null)
   const [audioStream, setAudioStream] = useState(null)
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(false)
+
   const isAudioAvailable = file || audioStream
 
   function handleAudioReset(){
@@ -22,9 +27,16 @@ function App() {
     <div className='flex flex-col max-w-[1000px mx-auto w-full]'>
       <section className='min-h-screen flex flex-col'>
         <Header />
-        {isAudioAvailable ? (
+        {output? (
+          <Information/>
+        ) : loading? (
+          <Transcribing/>
+        ) : isAudioAvailable? (
           <FileDisplay handleAudioReset={handleAudioReset} file={file} audioStream={setAudioStream}/>
-        ): (<HomePage setFile={setFile} setAudioStream={setAudioStream}/>)}
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream}/>
+        )}
+        
       </section>
       <h1 className='text-green-400'>Hello</h1>
       <footer></footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="flex items-center justify-between gap-4 p-4">
             <h1 className='font-medium'>Easy<span className='text-blue-400 bold'>Scribe</span></h1>
-            <button className='flex items-center gap-2 spacialBtn px-3 py-2 rounded-lg text-blue-400'><p>New</p><i className="fa-solid fa-plus"></i></button>
+            <button className='flex items-center gap-2 spacialBtn px-3 text-sm py-2 rounded-lg text-blue-400'><p>New</p><i className="fa-solid fa-plus"></i></button>
         </header>
     )
 }
