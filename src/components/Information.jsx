963e65b865a62881efe00604323484b9e45514f6
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-21 12:11:12 +0600

    toggle design between transcription and translation button

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -7,9 +7,9 @@ export default function Information() {
     <main className='flex-1 p-4 flex flex-col text-center gap-3 sm:gap-4 justify-center pb-20 max-w-prose w-full mx-auto'>
       <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
 
-      <div className='flex mx-auto bg-white border-2 border-solid border-blue-300 shadow rounded-full overflow-hidden items-center gap-2'>
-        <button className={'px-4 py-2 font-medium' + (tab === 'transcription' ? ' bg-blue-300 text-white' : ' text-blue-400 hover:text-blue-600')}>Transcription</button>
-        <button className='px-4 py-2 font-medium'>Translation</button>
+      <div className='grid grid-cols-2 mx-auto bg-white shadow rounded-full overflow-hidden items-center'>
+        <button onClick={() => setTab('transcription')} className={'px-4 duration-200 py-2 font-medium' + (tab === 'transcription' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}>Transcription</button>
+        <button onClick={() => setTab('translation')} className={'px-4 duration-200 py-2 font-medium' + (tab === 'translation' ? ' bg-blue-400 text-white' : ' text-blue-400 hover:text-blue-600')}>Translation</button>
       </div>
     </main>
   )
